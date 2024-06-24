0f0ad10c8fa8a0ae0f69797fe1b016f7ee6c236c
황재연 <hjng0825@gmail.com>
2024-06-24 16:10:54 +0900

    init: 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <Button>test</Button>
+      <div className="text-2xl text-red-500">test</div>
+    </>
+  )
+}
+
+export default App
