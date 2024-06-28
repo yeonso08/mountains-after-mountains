c0dd77bb63026d1be482bf47066611c17adf0350
황재연 <hjng0825@gmail.com>
2024-06-28 16:45:45 +0900

    feat : 카카오 로그인 비지니스 로직 추가

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,16 @@
 const Login = () => {
-  return <>login</>
+  const Rest_api_key = import.meta.env.VITE_REST_API_KEY //REST API KEY
+  const Redirect_uri = 'http://localhost:5173/auth' //Redirect URI
+  // oauth 요청 URL
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${Redirect_uri}&response_type=code`
+  const handleLogin = () => {
+    window.location.href = kakaoURL
+  }
+  return (
+    <>
+      <button onClick={handleLogin}>카카오 로그인</button>
+    </>
+  )
 }
 
 export default Login
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from '@/pages/login'
 import Test from '@/pages/Test'
+import KakaoRedirect from '@/pages/login/KakaoRedirect'
 
 const AppRoutes = () => {
   return (
@@ -8,6 +9,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="test" element={<Test />} />
         <Route path="/" element={<Login />} />
+        <Route path="/auth" element={<KakaoRedirect />} />
       </Routes>
     </Router>
   )
