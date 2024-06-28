7c5b7f389ed311586c765db3d19c1259305d6a9d
황재연 <hjng0825@gmail.com>
2024-06-28 18:00:07 +0900

    design : 등산일정 등록 UI 작업 중

--- src/pages/schedule/register/index.tsx
+++ src/pages/schedule/register/index.tsx
@@ -1,18 +1,27 @@
 import FooterButton from '@/components/common/button/FooterButton'
 import Header from '@/components/layouts/header'
+import { Select, SelectTrigger } from '@/components/ui/select'
+import { AsteriskIcon } from '@/icons'
 
 const RegisterSchedule = () => {
-    return (
-        <div className="flex h-full flex-col">
-            <Header title="등산일정 등록" />
-            <div className="flex h-full flex-col justify-between p-5">
-                <div>
-                    <div className="text-b1">어떤산에 가시나요?</div>
-                </div>
-                <FooterButton>일정 등록하기</FooterButton>
-            </div>
+  return (
+    <div className="flex h-full flex-col">
+      <Header title="등산일정 등록" />
+      <div className="flex h-full flex-col justify-between p-5">
+        <div className="">
+          <div className="flex text-b1">
+            어떤 산에 가시나요?
+            <AsteriskIcon />
+          </div>
+          <Select>
+            <SelectTrigger className="rounded-xl bg-white">test</SelectTrigger>
+          </Select>
         </div>
-    )
+
+        <FooterButton>일정 등록하기</FooterButton>
+      </div>
+    </div>
+  )
 }
 
 export default RegisterSchedule
