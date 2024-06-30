5e855b5ac0ea1f9bf776ad31fb3fa7ac6137c245
gyulhana <matearz@korea.ac.kr>
2024-06-30 19:02:33 +0900

    feat: 난이도 별점 컴포넌트 구현

---
+++ src/components/common/StarRate.tsx
@@ -0,0 +1,20 @@
+import Star from '@/assets/icons/star.svg?react'
+
+type Props = {
+  difficulty: number
+}
+
+const StarRate = ({ difficulty }: Props) => {
+  const rateArray = new Array(3).fill(0)
+
+  return (
+    <div className="flex items-center">
+      <div className="text-b3 text-gray-700">난이도</div>
+      {rateArray.map((_, index) => (
+        <Star key={index} color={index < difficulty ? '#0E9F59' : '#B0B0B0'} width={18} height={18} />
+      ))}
+    </div>
+  )
+}
+
+export default StarRate
