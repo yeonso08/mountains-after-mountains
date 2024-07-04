06fb900b498986edadcdaeca21df47bb16e90d67
황재연 <hjng0825@gmail.com>
2024-07-04 17:06:39 +0900

    feat : Redirect_uri 현재 주소로 설정

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,6 @@
 const Login = () => {
   const Rest_api_key = import.meta.env.VITE_REST_API_KEY //REST API KEY
-  const Redirect_uri = 'http://localhost:5173/auth' //Redirect URI
+  const Redirect_uri = `${window.location.origin}/auth`
   // oauth 요청 URL
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${Redirect_uri}&response_type=code`
   const handleLogin = () => {
