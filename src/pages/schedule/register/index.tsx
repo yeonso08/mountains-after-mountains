50662259cc97f17471adf222b44680ab75e7af11
황재연 <hjng0825@gmail.com>
2024-07-04 14:34:56 +0900

    chore : github action 테스트

--- src/pages/schedule/register/index.tsx
+++ src/pages/schedule/register/index.tsx
@@ -2,17 +2,17 @@ import FooterButton from '@/components/common/button/FooterButton'
 import Header from '@/components/layouts/header'
 
 const RegisterSchedule = () => {
-    return (
-        <div className="flex h-full flex-col">
-            <Header title="등산일정 등록" />
-            <div className="flex h-full flex-col justify-between p-5">
-                <div>
-                    <div className="text-b1">어떤산에 가시나요?</div>
-                </div>
-                <FooterButton>일정 등록하기</FooterButton>
-            </div>
+  return (
+    <div className="flex h-full flex-col">
+      <Header title="등산일정 등록" />
+      <div className="flex h-full flex-col justify-between p-5">
+        <div>
+          <div className="text-b1">어떤산에 가시나요?</div>
         </div>
-    )
+        <FooterButton>일정 등록하기</FooterButton>
+      </div>
+    </div>
+  )
 }
 
 export default RegisterSchedule
