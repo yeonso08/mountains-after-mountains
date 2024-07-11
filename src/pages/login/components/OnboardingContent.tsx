42d4dd32593657a233b7c6b00e227ec5bdb61306
황재연 <hjng0825@gmail.com>
2024-07-11 14:27:08 +0900

    feat : 로그인 carousel 추가

--- src/pages/login/components/OnboardingContent.tsx
+++ src/pages/login/components/OnboardingContent.tsx
@@ -5,12 +5,12 @@ interface OnboardingContentProps {
 
 const OnboardingContent = ({ img, text }: OnboardingContentProps) => {
   return (
-    <div className="h-64 overflow-hidden rounded-2xl">
+    <div className="h-[344px] overflow-hidden rounded-2xl">
       <div className="h-4/5 bg-black">
         <img src={img} alt="Onboarding image" className="h-full w-full object-cover" />
       </div>
       <div
-        className="flex h-1/5 items-center justify-center bg-white p-4 text-center"
+        className="flex h-1/5 items-center justify-center bg-white text-center text-b2"
         style={{ whiteSpace: 'pre-line' }}
       >
         {text}
