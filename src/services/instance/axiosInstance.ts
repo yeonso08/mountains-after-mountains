7bec6f8983b701979ee7c44a9bedb0233a3190e8
yeonso08 <hjng0825@gmail.com>
2024-07-20 02:25:28 +0000

    fix : 주소에서 api 제거

--- src/services/instance/axiosInstance.ts
+++ src/services/instance/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = `${import.meta.env.VITE_APP_API_URL}/api`
+const API_URL = `${import.meta.env.VITE_APP_API_URL}`
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
@@ -9,7 +9,7 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   function (config) {
-    const token = sessionStorage.getItem('authToken')
+    const token = sessionStorage.getItem('token')
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
