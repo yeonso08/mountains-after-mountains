5627fcc597e9b7b88a4f83e5a1c9e55a12e552e8
yeonso08 <https://github.com/yeonso08>
2024-08-22 06:24:10 +0000

    fix : 등산 일정 ui 수정

--- src/pages/schedule/list/index.tsx
+++ src/pages/schedule/list/index.tsx
@@ -13,10 +13,10 @@ const ListSchedule = () => {
     refetchOnWindowFocus: false,
   })
   return (
-    <div className="flex flex-col">
+    <div className="flex h-full flex-col">
       {isFetching && <LoadingSpinner />}
       <Header selected="schedule" />
-      <div className="flex h-screen flex-col gap-3 bg-background p-5">
+      <div className="flex flex-col gap-3 bg-background p-5">
         {data && data.length > 0 ? (
           data.map((schedule: ScheduleListResponse, index: number) => <ListCard key={index} schedule={schedule} />)
         ) : (
