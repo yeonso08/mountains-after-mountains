33c60ac4c9b5b11617096bbc2c51e79d97d2e88c
yeonso08 <https://github.com/yeonso08>
2024-09-12 06:51:14 +0000

    fix : vercel Analytics react로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Analytics } from '@vercel/analytics/next'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Analytics />
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
