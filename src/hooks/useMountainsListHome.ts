6900619edaac808b0a31cca2d9c6427ca6a42b75
yeonso08 <https://github.com/yeonso08>
2024-11-01 01:10:34 +0000

    feat : 에러페이지 추가

--- src/hooks/useMountainsListHome.ts
+++ src/hooks/useMountainsListHome.ts
@@ -3,16 +3,17 @@ import axiosInstance from '@/services/instance/axiosInstance'
 import { MountainListResponse } from '@/types/schedule'
 
 const useMountainsListHome = (keyword?: string) => {
-  const { data, isError, isLoading } = useQuery({
+  const { data, isError, isLoading, refetch } = useQuery({
     queryKey: ['mountainsList', keyword],
     queryFn: async () =>
       await axiosInstance.get<MountainListResponse[]>('/main/list', {
         params: { keyword },
       }),
     refetchOnWindowFocus: false,
+    retry: 0,
   })
 
-  return { data: data?.data, isError, isLoading }
+  return { data: data?.data, isError, isLoading, refetch }
 }
 
 export default useMountainsListHome
