864666a4408882e4d7bf90a643f203ac4cf4da81
yeonso08 <https://github.com/yeonso08>
2024-11-05 08:44:34 +0000

    feat : 회원탈퇴 userId 추가

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -2,16 +2,17 @@ import create from 'zustand'
 
 interface AuthState {
   isAuthenticated: boolean
-  login: (token: string, nickname: string, kakaoRefreshToken: string) => void
+  login: (token: string, nickname: string, kakaoRefreshToken: string, userId: string) => void
   logout: () => void
 }
 
 const useAuthStore = create<AuthState>(set => ({
   isAuthenticated: !!localStorage.getItem('token'),
-  login: (token, nickname, kakaoRefreshToken) => {
+  login: (token, nickname, kakaoRefreshToken, userId) => {
     localStorage.setItem('token', token)
     localStorage.setItem('nickName', nickname)
     localStorage.setItem('kakaoRefreshToken', kakaoRefreshToken)
+    localStorage.setItem('userId', userId)
     set({ isAuthenticated: true })
   },
   logout: () => {
@@ -19,6 +20,7 @@ const useAuthStore = create<AuthState>(set => ({
     localStorage.removeItem('nickName')
     localStorage.removeItem('scheduleId')
     localStorage.removeItem('kakaoRefreshToken')
+    localStorage.removeItem('userId')
     set({ isAuthenticated: false })
   },
 }))
